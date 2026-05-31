5a5ae86d5a90540504d74e8c5b26db0ca4b704a8
Emi-Condori <econdoriaduviri@frba.utn.edu.ar>
2026-05-31 17:40:17 -0300

    agrego clases para analitica de metricas

---
+++ incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/models/entities/persona/MetricaMensual.java
@@ -0,0 +1,9 @@
+package ar.edu.utn.frba.ddsi.models.entities.persona;
+
+import java.time.YearMonth;
+
+public class MetricaMensual {
+    private YearMonth periodo;
+    private Integer CantidadDonaciones;
+    private Integer OrganizacionesAyudadas;
+}
