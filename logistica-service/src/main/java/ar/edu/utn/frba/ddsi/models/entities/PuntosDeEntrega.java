2e331a802f47ea14083acfb7a0fc488d5562ffa7
Eugenia Perez <eugeniaagueda@gmail.com>
2026-07-06 21:30:10 -0300

    fix PuntosDeEntrega

--- logistica-service/src/main/java/ar/edu/utn/frba/ddsi/models/entities/PuntosDeEntrega.java
+++ logistica-service/src/main/java/ar/edu/utn/frba/ddsi/models/entities/PuntosDeEntrega.java
@@ -3,7 +3,7 @@ package ar.edu.utn.frba.ddsi.models.entities;
 public class PuntosDeEntrega {
     private Long id;
     public Integer orden;
-    public EntidadBeneficiaria entidadBeneficiaria;
+    public Long entidadBeneficiariaId;
     public Entrega entrega;
 
 }
